7b6652c5c295323d5dd79c7f3f2f3719fce342b9
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-12-01 10:29:12 -0300

    separate modal, tools and erros to appropriate class

--- stage05/calculoDoIMC/assets/js/Modal.js
+++ stage05/calculoDoIMC/assets/js/Modal.js
@@ -4,15 +4,39 @@ export const Modal = {
     resultMessage: document.querySelector('h2'),
     btnClose: document.querySelector('#closeModal'),
     showModalWrapper() {
-        Modal.wrapper.classList.add('show');
+        //this.wrapper.classList.add('show')
+        this.wrapper.classList.add('show')
     },
     hideModalWrapper() {
-        Modal.wrapper.classList.remove('show');
+        this.wrapper.classList.remove('show')
     },
     showErrorMessage() {
-        Modal.errorMessage.classList.add('show');
+        this.errorMessage.classList.add('show')
+        setTimeout(() => { Modal.hideErrorMessage() }, 4000)
     },
     hideErrorMessage() {
-        Modal.errorMessage.classList.remove('show');
+        this.errorMessage.classList.remove('show')
     },
+    closeModalOnClick() {
+        this.hideModalWrapper()
+    },
+}
+
+// handle escape key from keyboard
+document.addEventListener('keydown', (event) => {
+    let keyPressed = event.key
+
+    if (keyPressed === 'Escape' && Modal.wrapper.classList.contains('show')) {
+        Modal.wrapper.classList.remove('show')
+    }
+})
+
+// Show popup (modal) with IMC calculate result
+export const ShowResult = (result) => {
+    Modal.showModalWrapper()
+    Modal.resultMessage.innerText = `Seu IMC é de ${result}`
+
+    Modal.btnClose.onclick = () => {
+        Modal.closeModalOnClick()
+    }
 }
--- stage05/calculoDoIMC/assets/js/app.js
+++ stage05/calculoDoIMC/assets/js/app.js
@@ -1,52 +1,22 @@
-import { Modal } from './Modal.js';
+import { Modal, ShowResult } from './Modal.js'
+import { CalculateIMC } from './Tools.js'
+import { CheckIfWrongEntry } from './Errors.js'
  
 // Capture elements
-const form = document.querySelector('form');
-const inputWeight = document.querySelector('#weight');
-const inputHeight = document.querySelector('#height');
-const btnCalc = document.querySelector('#calc');
-
-
-// handle close button on result popup (modal)
-Modal.btnClose.onclick = () => {
-    inputWeight.value = '';
-    inputHeight.value = '';
-    Modal.hideModalWrapper();
-};
-
-// handle escape key from keyboard
-document.addEventListener('keydown', (event) => {
-    let keyPressed = event.key;
-
-    if (keyPressed === 'Escape' && Modal.wrapper.classList.contains('show')) {
-        inputWeight.value = '';
-        inputHeight.value = '';
-        Modal.wrapper.classList.remove('show');
-    };
-});
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
 
 // Handle form submit
 form.onsubmit = event => {
-    event.preventDefault();
-
-    if (inputWeight.value < 1 || inputHeight.value < 1) {
-        Modal.showErrorMessage();
-
-        setTimeout(() => { Modal.hideErrorMessage(); }, 4000);
+    event.preventDefault()
+    
+    const evalTextBox = CheckIfWrongEntry(inputWeight.value, inputHeight.value)
+    
+    if (evalTextBox) {
+        Modal.showErrorMessage()
     } else {
-        showResult();
+        const result = CalculateIMC(inputWeight.value, inputHeight.value)
+        ShowResult(result)
     }
-}
-
-// Calculate IMC
-function calculateIMC(weight, height) {
-    return Math.round(weight / ((height/100) ** 2));
-}
-
-// Show popup (modal) with IMC calculate result
-function showResult() {
-    const result = calculateIMC(inputWeight.value, inputHeight.value);
-        
-    Modal.showModalWrapper();
-    Modal.resultMessage.innerText = `Seu IMC é de ${result}`;
 }
