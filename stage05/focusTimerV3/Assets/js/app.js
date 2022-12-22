0d6d1b607c01f4c9ee992ded8a9b36eabd3ceed6
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-12-22 00:10:43 -0300

    added comments and css hover effect

--- stage05/focusTimerV3/Assets/js/app.js
+++ stage05/focusTimerV3/Assets/js/app.js
@@ -33,14 +33,14 @@ Env.buttonMinusFiveMinutes.addEventListener('click', () => {
     Controls.minusFiveMinutes()
 })
 
+
+// themes buttons #############################################
+
+
 Env.buttonForest.addEventListener('click', () => {
     Sound.bgMusicForest()
     Sound.buttonClick()
     Themes.forest()
-})
-
-Env.volumeForest.addEventListener('input', () => {
-    Sound.bgAudioForest.volume = Env.volumeForest.value
 })
 
 Env.buttonRain.addEventListener('click', () => {
@@ -49,24 +49,30 @@ Env.buttonRain.addEventListener('click', () => {
     Themes.rain()
 })
 
-Env.buttonRain.addEventListener('input', () => {
-    Sound.bgAudioRain.volume = Env.volumeRain.value
-})
-
 Env.buttonCoffee.addEventListener('click', () => {
     Sound.bgMusicCoffee()
     Sound.buttonClick()
     Themes.coffee()
 })
 
-Env.buttonCoffee.addEventListener('input', () => {
-    Sound.bgAudioCoffee.volume = Env.volumeCoffee.value
-})
-
 Env.buttonFireplace.addEventListener('click', () => {
     Sound.bgMusicFireplace()
     Sound.buttonClick()
     Themes.fireplace()
+})
+
+// Themes volume sliders ######################################
+
+Env.volumeForest.addEventListener('input', () => {
+    Sound.bgAudioForest.volume = Env.volumeForest.value
+})
+
+Env.buttonRain.addEventListener('input', () => {
+    Sound.bgAudioRain.volume = Env.volumeRain.value
+})
+
+Env.buttonCoffee.addEventListener('input', () => {
+    Sound.bgAudioCoffee.volume = Env.volumeCoffee.value
 })
 
 Env.buttonFireplace.addEventListener('input', () => {
