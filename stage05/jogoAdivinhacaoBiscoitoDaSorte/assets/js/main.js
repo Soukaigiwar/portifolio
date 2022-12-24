36d808adc074c4d63371215c551cbae1b83b3227
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-12-24 00:08:04 -0300

    add js click event

--- stage05/jogoAdivinhacaoBiscoitoDaSorte/assets/js/main.js
+++ stage05/jogoAdivinhacaoBiscoitoDaSorte/assets/js/main.js
@@ -1,1 +1,14 @@
-console.log("jogo da adivinhação");
+const openOtherFortuneCookie = document.querySelector('button')
+const fortuneCookie = document.querySelector('.biscoito')
+const mainScreen = document.querySelector('.antes')
+const resultScreen = document.querySelector('.depois')
+
+fortuneCookie.addEventListener('click', () => {
+    mainScreen.classList.toggle('hide')
+    resultScreen.classList.toggle('hide')
+})
+
+openOtherFortuneCookie.addEventListener('click', () => {
+    mainScreen.classList.toggle('hide')
+    resultScreen.classList.toggle('hide')
+})
