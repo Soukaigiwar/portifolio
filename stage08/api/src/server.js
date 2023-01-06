d63edfe869f509d2cf3cfc79d99f710faca21abe
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-01-05 23:21:54 -0300

    create method post to send a json to api

--- stage08/api/src/server.js
+++ stage08/api/src/server.js
@@ -2,11 +2,7 @@ const { application, response, request } = require('express')
 const express = require('express')
 
 const app = express()
-const PORT = 3333
-
-app.listen(PORT, () => {
-    console.log(`Server running on Port ${PORT}.`)
-})
+app.use(express.json())
 
 app.get("/", (request, response) => {
     response.send('hello world')
@@ -23,14 +19,25 @@ app.get("/product/:id", (request, response) => {
 app.get("/product/:id/:unit_price", (request, response) => {
     const { id, unit_price } = request.params
     response.send(`
-        Id do produto: ${id}.
-        Preço do produto: R$ ${unit_price}.
-        `)
+    Id do produto: ${id}.
+    Preço do produto: R$ ${unit_price}.
+    `)
 })
 
 app.get("/user", (request, response) => {
     const { id, rule } = request.query
-
+    
     response.send(`Id: ${id}, tipo: ${rule}.`)
     console.log(id, rule);
+})
+
+app.post("/user", (request, response) => {
+    const { name, email, passw } = request.body
+    
+    response.send(`Você, ${name} que tem o email ${email} e senha ${passw} chamou o POST.`)
+})
+
+const PORT = 3333
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}.`)
 })
