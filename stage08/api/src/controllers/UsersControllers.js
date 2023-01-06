e41ed7f2798efbc090a6cd8e6b114fe4a74d0a93
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-01-06 01:15:21 -0300

    Create Utils/AppErrors.js

--- stage08/api/src/controllers/UsersControllers.js
+++ stage08/api/src/controllers/UsersControllers.js
@@ -1,3 +1,5 @@
+const AppError = require("../utils/AppError")
+
 class UsersControllers {
     /**
      * index - GET para listar vários registros.
@@ -9,7 +11,11 @@ class UsersControllers {
 
     create(request, response) {
         const { name, email, passw } = request.body //json
-    
+        
+        if (!name) {
+            throw new AppError("Nome é obrigatório!")
+        }
+        
         response.status(201).json({ name, email, passw }) //response json
     }
 }
