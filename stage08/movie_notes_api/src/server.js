1d077852529e5f13b0b9e35beeb6ab6ac6d4b460
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-01-10 01:21:09 -0300

    Post - User Create API

--- stage08/movie_notes_api/src/server.js
+++ stage08/movie_notes_api/src/server.js
@@ -1,10 +1,11 @@
 const express = require("express")
 
+const routes = require("./routes")
+
 const app = express()
+app.use(express.json())
+app.use(routes)
+
+
 const PORT = 3000
-
-app.get("/", (request, response) => {
-    response.send("init project")
-})
-
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`))
