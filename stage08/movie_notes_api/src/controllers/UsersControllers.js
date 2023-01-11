074cf806416ee0404bd02c2d30ef9bf8279d7f1b
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-01-11 19:55:34 -0300

    added update users in database controller

--- stage08/movie_notes_api/src/controllers/UsersControllers.js
+++ stage08/movie_notes_api/src/controllers/UsersControllers.js
@@ -1,8 +1,9 @@
-const { hash } = require("bcryptjs")
+const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 const sqliteConnection = require("../database/sqlite")
 
 class UsersControllers {
+
     async create(request, response) {
         const { name, email, password } = request.body
 
@@ -15,11 +16,59 @@ class UsersControllers {
         }
 
         const hashedPassword = await hash(password, 8)
+
         await database.run(
             "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
             [name, email, hashedPassword])
 
         return response.status(201).json()
+    }
+
+    async update(request, response) {
+        const { name, email, old_password, new_password } = request.body
+        const { id } = request.params
+
+        const database = await sqliteConnection()
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+        
+        if (!user) {
+            throw new AppError("Usuário não encontrado.")
+        }
+        
+        const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+        
+        if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+            throw new AppError("Este email já está em uso.")
+        }
+        
+        user.name = name
+        user.email = email
+        
+        if (new_password && !old_password) {
+            throw new AppError("Precisa informar a senha antiga.")
+        }
+
+        const validateOldPassword = await compare(old_password, user.password)
+        
+        if (!validateOldPassword) {
+            throw new AppError("Senha nao confere.")
+        }
+        
+        if (new_password && old_password) {
+            user.password = await hash(new_password, 8)
+        }
+
+        await database.run(`
+        UPDATE users SET
+        name = ?,
+        email = ?,
+        password = ?,
+        updated_at = DATETIME('now')
+        WHERE id = ?`,
+        [user.name, user.email, user.password, id]
+        )
+        
+        return response.json()
     }
 
 
--- stage08/movie_notes_api/src/routes/users.routes.js
+++ stage08/movie_notes_api/src/routes/users.routes.js
@@ -7,13 +7,14 @@ const userController = new UsersControllers()
 const usersRoutes = Router()
 
 function myMiddleware(request, response, next) {
-    if (!request.body.isAdmin) {
-        return response.json( { message: "User unauthorized "})
-    }
+    // if (!request.body.isAdmin) {
+    //     return response.json( { message: "User unauthorized "})
+    // }
 
     next()
 }
 
-usersRoutes.post("/", myMiddleware, userController.create)
+usersRoutes.post("/", userController.create)
+usersRoutes.put("/:id", userController.update)
 
 module.exports = usersRoutes
