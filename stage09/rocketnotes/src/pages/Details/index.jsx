7db6b063bbbae31e1155d3ac4d2126033fdebc64
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-02-09 22:36:53 -0300

    init stage09

---
+++ stage09/rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles.js"
+
+export function Details() {
+    return (
+        <Container>
+            <h3>Sergio HP Mello</h3>
+        </Container>
+    )
+  }
+  
