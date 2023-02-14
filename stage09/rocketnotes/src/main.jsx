cb0f08e589b2b0d0fce8eb965768e661be0e2cd2
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-02-13 22:03:44 -0300

    added other components

--- stage09/rocketnotes/src/main.jsx
+++ stage09/rocketnotes/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details'
+import { Home } from './pages/Home'
+// import { Details } from './pages/Details'
 import { ThemeProvider } from 'styled-components'
 
 import theme from './styles/theme'
@@ -10,7 +11,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-        <Details />
+        <Home />
+        {/* <De1tails /> */}
     </ThemeProvider>
   </React.StrictMode>,
 )
--- stage09/rocketnotes/src/pages/Details/index.jsx
+++ stage09/rocketnotes/src/pages/Details/index.jsx
@@ -1,14 +1,49 @@
-import { Container } from "./styles.js"
+import { Container, Links, Content } from "./styles.js"
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header/"
+import { Section } from "../../components/Section/"
+import { Tag } from "../../components/Tag/index.jsx"
+import { ButtonText } from "../../components/ButtonText/index.jsx"
 
 export function Details() {
     return (
         <Container>
             <Header />
-            
-            
+
+            <main>
+                <Content>
+                    <ButtonText title="Excluir nota" />
+
+                    <h1>
+                        Introdução ao React
+                    </h1>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Vel aut minus ipsa molestiae natus. Veritatis,
+                        cupiditate inventore, optio pariatur voluptatibus
+                        ratione consequatur magnam enim voluptate recusandae
+                        consequuntur, non eum minima.
+                        Ab nisi consequatur aliquid inventore, temporibus
+                        voluptates veritatis nemo quos explicabo vero corporis
+                        nulla a, officiis, repudiandae asperiores ullam. Ducimus
+                        aspernatur necessitatibus illum laboriosam dignissimos
+                        repellat reprehenderit, at harum cupiditate!
+                    </p>
+                    <Section title="Links Úteis">
+                        <Links>
+                            <li><a href="#">www.rocketseat.com.br</a></li>
+                            <li><a href="#">www.sergiomello.online</a></li>
+                        </Links>
+                    </Section>
+                    <Section title="Marcadores">
+                        <Tag title="Express" />
+                        <Tag title="NodeJs" />
+                    </Section>
+
+                    <Button title="Voltar" />
+                </Content>
+            </main>
         </Container>
     )
-  }
-  
+}
