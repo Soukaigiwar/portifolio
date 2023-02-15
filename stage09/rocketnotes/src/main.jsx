eebd86a459fe74d3ffd2311dba4963ebac844761
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-02-14 23:33:31 -0300

    added SignUp page

--- stage09/rocketnotes/src/main.jsx
+++ stage09/rocketnotes/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
 
 import { ThemeProvider } from 'styled-components'
 
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-        <SignIn />
+        <SignUp />
         {/* <De1tails /> */}
     </ThemeProvider>
   </React.StrictMode>,
--- stage09/rocketnotes/src/pages/SignUp/index.jsx
+++ stage09/rocketnotes/src/pages/SignUp/index.jsx
@@ -1,17 +1,23 @@
-import { FiMail, FiLock } from "react-icons/fi"
+import { FiMail, FiLock, FiUser } from "react-icons/fi"
 import { Container, Form, Background } from "./styles"
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 
-export function SignIn() {
+export function SignUp() {
     return (
         <Container>
+            <Background />
             <Form>
                 <h1>Rocket Notes</h1>
                 <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-                <h2>Faça seu login</h2>
+                <h2>Crie sua conta</h2>
 
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
                 <Input
                     placeholder="E-mail"
                     type="text"
@@ -22,10 +28,9 @@ export function SignIn() {
                     type="password"
                     icon={FiLock}
                 />
-                <Button title="Entrar" />
-                <a href="#">Criar conta</a>
+                <Button title="Cadastrar" />
+                <a href="#">Voltar para o login</a>
             </Form>
-            <Background />
         </Container>
     )
 }
