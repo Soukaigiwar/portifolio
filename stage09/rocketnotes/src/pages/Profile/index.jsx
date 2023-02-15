0f92a6a8206ff4fc1b91b1e0c73cdcccd84dd5aa
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-02-15 00:13:34 -0300

    added Profile page

---
+++ stage09/rocketnotes/src/pages/Profile/index.jsx
@@ -0,0 +1,53 @@
+import { Container, Form, Avatar } from "./styles.js"
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+import { FiArrowLeft, FiMail, FiLock, FiUser, FiCamera } from "react-icons/fi"
+
+export function Profile() {
+    return (
+        <Container>
+            <header>
+                <a href="/">
+                    <FiArrowLeft />
+                </a>
+            </header>
+
+            <Form>
+                <Avatar>
+                    <img
+                        src="https://github.com/soukaigiwar.png"
+                        alt="Foto do usuário"
+                    />
+                    <label htmlFor="avatar">
+                        <FiCamera />
+                        <Input
+                            id="avatar"
+                            type="file"
+                        />
+                    </label>
+                </Avatar>
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+                <Input
+                    placeholder="Senha atual"
+                    type="password"
+                    icon={FiLock}
+                />
+                <Input
+                    placeholder="Nova senha"
+                    type="password"
+                    icon={FiLock}
+                />
+                <Button title="Salvar"/>
+            </Form>
+        </Container>
+    )
+}
