18fda6cde16b83c5461c4e4078ff394e90193122
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-02-22 21:24:15 -0300

    create SignUp page

---
+++ stage09/rocketmovies/src/pages/SignUp/index.jsx
@@ -0,0 +1,41 @@
+import { FiMail, FiLock, FiUser, FiArrowLeft } from "react-icons/fi"
+import { Button } from "../../components/Button"
+import { Input } from "../../components/Input"
+import { Container, Form, Background } from './styles'
+
+export function SignUp() {
+    return (
+        <Container>
+            <Form>
+                <h1>RocketMovies</h1>
+                <p>Aplicação para acompanhar tudo que assistir.</p>
+
+                <div>
+                    <h2>Crie sua conta</h2>
+
+                    <Input
+                        placeholder="Nome"
+                        type="text"
+                        icon={FiUser}
+                    />
+                    <Input
+                        placeholder="E-mail"
+                        type="text"
+                        icon={FiMail}
+                    />
+                    <Input
+                        placeholder="Senha"
+                        type="password"
+                        icon={FiLock}
+                    />
+                    <Button title="Cadastrar" style={{
+                        heigh: "5.6rem",
+                        width: "34rem",
+                    }} />
+                </div>
+                <a href="#"><FiArrowLeft />Voltar para o Login</a>
+            </Form>
+            <Background />
+        </Container>
+    )
+}
