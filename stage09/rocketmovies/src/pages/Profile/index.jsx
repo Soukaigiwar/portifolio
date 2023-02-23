9e049eb408446884b5fdbf849498c33af3874bcf
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-02-22 21:59:15 -0300

    create Profile page

---
+++ stage09/rocketmovies/src/pages/Profile/index.jsx
@@ -0,0 +1,51 @@
+import { FiArrowLeft, FiCamera, FiUser, FiMail, FiLock } from "react-icons/fi"
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+import { Container, Form, Avatar } from "./styles"
+
+export function Profile() {
+    return (
+        <Container>
+            <header>
+                <a href="#"><FiArrowLeft />Voltar</a>
+            </header>
+
+            <Form>
+                <Avatar>
+                    <img
+                        src="https://github.com/soukaigiwar.png"
+                        alt="Foto do Usuário"
+                    />
+                    <label htmlFor="avatar">
+                        <FiCamera />
+                        <Input
+                            id="avatar"
+                            type="file"
+                        />
+                    </label>
+                </Avatar>
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+                <Input
+                    placeholder="Senha atual"
+                    type="password"
+                    icon={FiLock}
+                />
+                <Input
+                    placeholder="Nova senha"
+                    type="password"
+                    icon={FiLock}
+                />
+                <Button title="Salvar"/>
+            </Form>
+        </Container>
+    )
+}
