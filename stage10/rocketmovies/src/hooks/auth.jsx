0ce5ef7fa49491515b04d0cd781feed66a488cfa
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-03-11 22:41:55 -0300

    set and get localstorage user login

--- stage10/rocketmovies/src/hooks/auth.jsx
+++ stage10/rocketmovies/src/hooks/auth.jsx
@@ -1,14 +1,23 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState, useEffect } from "react"
 import { api } from '../services/api'
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+    const [data, setData] = useState({})
 
     async function signIn({ email, password }) {
         try {
             const response = await api.post("/sessions", { email, password })
-            console.log(response);
+            const { user, token } = response.data
+
+            localStorage.setItem("@rocketmovies:user", JSON.stringify(user))
+            localStorage.setItem("@rocketmovies:token", token)
+
+            api.defaults.headers.authorization = `Bearer ${token}`
+            setData({ user, token })
+
+            
         } catch (error) {
             if (error.response) {
                 alert(error.response.data.message)
@@ -18,8 +27,25 @@ function AuthProvider({ children }) {
         }
     }
 
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketmovies:token")
+        const user = localStorage.getItem("@rocketmovies:user")
+
+        console.log(token && user);
+
+        if (token) {
+            console.log("entrou", user);
+            api.defaults.headers.authorization = `Bearer ${token}`
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            })
+        }
+    }, [])
+
     return (
-        <AuthContext.Provider value={{ signIn }}>
+        <AuthContext.Provider value={{ signIn, user: data.user }}>
             {children}
         </AuthContext.Provider>
     )
