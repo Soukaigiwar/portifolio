6be1a01088b5688ca55a7b62b92c40f08275120a
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-03-19 22:43:20 -0300

    addNewTag feature init

--- stage10/rocketmovies/src/pages/CreateMovie/index.jsx
+++ stage10/rocketmovies/src/pages/CreateMovie/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Container, Form } from "./styles"
 import { FiSearch } from "react-icons/fi"
 import { BackButton } from "../../components/BackButton"
@@ -9,44 +10,52 @@ import { Header } from "../../components/Header"
 
 
 export function CreateMovie() {
+    const [tags, setTags] = useState([])
+    const [newTag, setNewTag] = useState("")
+
+    function handleAddTag() {
+        setTags(prevState => [...prevState, newTag])
+        setNewTag("")
+    }
+
     return (
         <Container>
             <Header>
-                <Input icon={FiSearch} placeholder="Pesquisar pelo título"/>
+                <Input icon={FiSearch} placeholder="Pesquisar pelo título" />
             </Header>
 
             <main>
                 <Form>
                     <BackButton />
-                    
+
                     <h2>Novo Filme</h2>
                     <div className="double_input">
-                        <Input placeholder="Título"/>
-                        <Input placeholder="Sua nota (de 0 a 5)"/>
+                        <Input placeholder="Título" />
+                        <Input placeholder="Sua nota (de 0 a 5)" />
                     </div>
                     <Textarea placeholder="Observações" />
                     <h3>Marcadores</h3>
                     <div className="tag_item">
-                        <TagItem
-                            value="react"
-                        />
-                        <TagItem
-                            value="JavaScript"
-                        />
-                        <TagItem
-                            value="JavaScript"
-                        />
-                        <TagItem
-                            value="JavaScript"
-                        />
+                        {
+                            tags.map((tag, index) => (
+                                <TagItem
+                                    key={String(index)}
+                                    value={tag}
+                                    onClick={() => { }}
+                                />
+                            ))
+                        }
                         <TagItem
                             isNew
                             placeholder="Novo marcador"
+                            onChange={e => setNewTag(e.target.value)}
+                            value={newTag}
+                            onClick={handleAddTag}
                         />
                     </div>
                     <div className="button_area">
                         <button>Excluir filme</button>
-                        <Button title="Salvar alterações"/>
+                        <Button title="Salvar alterações" />
                     </div>
                 </Form>
             </main>
