30321cc55aaf3c93129aed9ad5c4475c3f5e0648
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-03-28 23:58:17 -0300

    refactoring routes

--- stage10/rocketnotes_api/src/server.js
+++ stage10/rocketnotes_api/src/server.js
@@ -1,39 +1,11 @@
-const { request } = require("express")
 const express = require("express")
 
-const app = express()
+const routes = require("./routes")
 
+const app = express()
 app.use(express.json())
 
-app.get("/", (request, response) => {
-    response.send("hello world!")
-}) 
-
-// route params (valores obrigatórios)
-app.get("/message/:id/:user", (request, response) => {
-    const { id, user } = request.params
-
-    response.send(`
-        Mensagem Id: ${id}.<br />
-        Para Usuário: ${user}.
-    `)
-})
-
-// Query params (parâmetros não são obrigatórios)
-app.get("/user", (request, response) => {
-    const { page, limit } = request.query
-    
-    response.send(`
-        Página: ${page}.<br />
-        Mostrar ${limit}.`)
-})
-
-app.post("/users", (request, response) => {
-    const { name, email, password } = request.body
-
-    response.json({ name, email, password })
-})
+app.use(routes)
 
 const PORT = 3333
-
 app.listen(PORT, () => console.log(`Api running on port: ${PORT}`))
